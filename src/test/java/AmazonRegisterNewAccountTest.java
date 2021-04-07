3ec385a67e2adc1fa1cd85481fc5296bda8363f3
Mateusz <mateopolanski@gmail.com>
2021-04-07 03:29:10 +0200

    commit

--- src/test/java/AmazonRegisterNewAccountTest.java
+++ src/test/java/AmazonRegisterNewAccountTest.java
@@ -14,6 +14,7 @@ public class AmazonRegisterNewAccountTest {
     Random randomGenerator = new Random();
     public int randomInt = randomGenerator.nextInt(10000);
     public final String PASS = "Pass1234567890";
+    public final String OTP_MESSAGE = "//div[@class=\"a-section cvf-alert-section cvf-widget-alert-message\"]";
 
     String otpKeyStr = "6jm7n6xwitpjooh7ihewyyzeux7aqmw2"; // <- this 2FA secret key.
 
@@ -24,30 +25,33 @@ public class AmazonRegisterNewAccountTest {
     @BeforeEach
     public void navigateToHomepage() throws InterruptedException {
 
-        System.setProperty("webdriver.chrome.driver" , CHROME_DRIVER_PATH);
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(AMAZON_HOMEPAGE);
-    }
+                System.setProperty("webdriver.chrome.driver" , CHROME_DRIVER_PATH);
+                driver = new ChromeDriver();
+                driver.manage().window().maximize();
+                driver.get(AMAZON_HOMEPAGE);
+            }
 
-    @Test
-    public void registerNewAccount() {
+            @Test
+            public void registerNewAccount() {
 
-        WebElement signInBtn = driver.findElement(By.xpath("//span[@class=\"nav-line-2 nav-long-width\"]"));
-        signInBtn.click();
+                WebElement signInBtn = driver.findElement(By.xpath("//span[@class=\"nav-line-2 nav-long-width\"]"));
+                signInBtn.click();
 
-        driver.manage().timeouts().implicitlyWait(5 , TimeUnit.SECONDS);
-        WebElement newAccount = driver.findElement(By.xpath("//a[@class=\"a-button-text\"]"));
+                driver.manage().timeouts().implicitlyWait(5 , TimeUnit.SECONDS);
+                WebElement newAccount = driver.findElement(By.xpath("//a[@class=\"a-button-text\"]"));
 
-        newAccount.click();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.findElement(By.id("ap_customer_name")).sendKeys("Mateusz");
-        driver.findElement(By.id("ap_email")).sendKeys("usernamex" + randomInt + "@gmail.com");
-        driver.findElement(By.xpath("//input[@type=\"password\"]")).sendKeys(PASS);
-        driver.findElement(By.xpath("//input[@id=\"ap_password_check\"]")).sendKeys(PASS);
-        driver.findElement(By.className("a-button-input")).click();
-        driver.findElement(By.xpath("//input[@name=\"code\"]")).sendKeys(twoFactorCode);
-        driver.findElement(By.className("a-button-input")).click();
+                newAccount.click();
+                driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+                driver.findElement(By.id("ap_customer_name")).sendKeys("Mateusz");
+                driver.findElement(By.id("ap_email")).sendKeys("usernamex" + randomInt + "@gmail.com");
+                driver.findElement(By.xpath("//input[@type=\"password\"]")).sendKeys(PASS);
+                driver.findElement(By.xpath("//input[@id=\"ap_password_check\"]")).sendKeys(PASS);
+                driver.findElement(By.className("a-button-input")).click();
+                driver.findElement(By.xpath("//input[@name=\"code\"]")).sendKeys(twoFactorCode);
+                driver.findElement(By.className("a-button-input")).click();
+                WebElement message = driver.findElement(By.xpath(OTP_MESSAGE));
+                String alert = message.getText();
+                Assertions.assertEquals("Invalid OTP. Please check your code and try again.", alert);
 
 
 
